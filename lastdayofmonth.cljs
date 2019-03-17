c8750b68ab5f762368e3677b348360411a33d95b
Topi Santakivi <topi.santakivi@gmail.com>
2019-03-17 11:29:46 +0200

    ClojureScript

--- lastdayofmonth.cljs
+++ lastdayofmonth.cljs
@@ -1,5 +1,7 @@
 (def month-days (array 31 28 31 30 31 30 31 31 30 31 30 31))
 
+(def feb-leap-day 29)
+
 (def today
   { 
     :year  (.getFullYear (js/Date.)), 
@@ -7,10 +9,24 @@
     :day   (.getDate (js/Date.))
   })
 
+(defn february
+  [month]
+  (if (= month 1) true false)) ; month is 0-11
+
+(defn leap-year
+  [year]
+  (cond
+    (not= (mod year 4) 0) false
+    (not= (mod year 100) 0) true
+    (= (mod year 400) 0) true
+    :else false
+  ))
+
 (defn last-day-of-month
   [date]
-  (println (:year date)))
+  (if (and (february (:month date)) 
+           (leap-year (:year date)))
+    feb-leap-day
+    (nth month-days (:month date))))
 
-
-
-(last-day-of-month today)
+(println (str "Last day of month is " (last-day-of-month today)))
